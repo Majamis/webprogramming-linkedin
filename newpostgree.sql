e9b46a30a532f84ff95f14168e30845ac5609913
Majamis <omerzahidbajwa@gmail.com>
2013-11-15 17:40:47 +0500

    modified postgree sql

--- newpostgree.sql
+++ newpostgree.sql
@@ -6,16 +6,16 @@ DROP TABLE IF EXISTS "add_contact_notification";
 
 CREATE TABLE "add_contact_notification" (
   "id" SERIAL NOT NULL,
-  "userId" integer NOT NULL references users("userId"),
-  "contact_userId" integer NOT NULL references users("userId"),
+  "userid" integer NOT NULL references users("userid"),
+  "contact_userid" integer NOT NULL references users("userid"),
   "personal_note" varchar(255) DEFAULT NULL,
-  "relationId" varchar(255) NOT NULL,
+  "relationid" varchar(255) NOT NULL,
   "time_date" date NOT NULL,
   PRIMARY KEY ("id")
-  /*KEY "userId" ("userId"),
+  /*KEY "userId" ("userid"),
   KEY "contact_userId" ("contact_userid")
-  CONSTRAINT "add_contact_notification_ibfk_1" FOREIGN KEY ("userId") REFERENCES "users" ("userId"),
-  CONSTRAINT "add_contact_notification_ibfk_2" FOREIGN KEY ("contact_userId") REFERENCES "users" ("userId")
+  CONSTRAINT "add_contact_notification_ibfk_1" FOREIGN KEY ("userid") REFERENCES "users" ("userid"),
+  CONSTRAINT "add_contact_notification_ibfk_2" FOREIGN KEY ("contact_userid") REFERENCES "users" ("userid")
 */);
 
 /*Data for the table "add_contact_notification" */
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS "confirm";
 
 CREATE TABLE "confirm" (
   "id" SERIAL NOT NULL,
-  "userId" varchar(128) NOT NULL,
+  "userid" varchar(128) NOT NULL,
   "key" varchar(128) NOT NULL,
   "email" varchar(250) DEFAULT NULL,
   PRIMARY KEY ("id")
@@ -39,16 +39,16 @@ DROP TABLE IF EXISTS "contacts_info";
 
 CREATE TABLE "contacts_info" (
   "contactId" SERIAL NOT NULL,
-  "userId" integer DEFAULT NULL references users("userId"),
-  "fName" varchar(40) NOT NULL,
-  "lName" varchar(40) NOT NULL,
-  "userName" varchar(40) NOT NULL,
-  "email" varchar(40) NOT NULL,
-  "password" varchar(40) NOT NULL,
-  "type" varchar(40) NOT NULL,
+  "userIdd" integer DEFAULT NULL references users("userid"),
+  "FName" varchar(40) NOT NULL,
+  "LName" varchar(40) NOT NULL,
+  "UserName" varchar(40) NOT NULL,
+  "Email" varchar(40) NOT NULL,
+  "Password" varchar(40) NOT NULL,
+  "Type" varchar(40) NOT NULL,
   PRIMARY KEY ("contactId")
-  /*KEY "userId" ("userId"),
-  CONSTRAINT "userId" FOREIGN KEY ("userId") REFERENCES "users" ("userId") ON DELETE NO ACTION ON UPDATE NO ACTION
+  /*KEY "userId" ("userIdd"),
+  CONSTRAINT "userIdd" FOREIGN KEY ("userIdd") REFERENCES "users" ("userid") ON DELETE NO ACTION ON UPDATE NO ACTION
 */);
 
 /*Data for the table "contacts_info" */
@@ -67,42 +67,42 @@ CREATE TABLE "relation_types" (
 DROP TABLE IF EXISTS "useradditionalinfo";
 
 CREATE TABLE "useradditionalinfo" (
-  "userId" integer NOT NULL references users("userId"),
-  "country" varchar(255) NOT NULL,
-  "postalCode" integer NOT NULL,
-  "jTitle" varchar(255) NOT NULL,
-  "selfEmp" boolean NOT NULL,
-  "company" varchar(255) DEFAULT NULL,
-  "industry" varchar(255) DEFAULT NULL,
-  "typeId" varchar(255) NOT NULL,
-  "picture" varchar(255) DEFAULT NULL,
+  "UserId" integer NOT NULL references users("userid"),
+  "Country" varchar(255) NOT NULL,
+  "PostalCode" integer NOT NULL,
+  "JTitle" varchar(255) NOT NULL,
+  "SelfEmp" boolean NOT NULL,
+  "Company" varchar(255) DEFAULT NULL,
+  "Industry" varchar(255) DEFAULT NULL,
+  "TypeId" varchar(255) NOT NULL,
+  "Picture" varchar(255) DEFAULT NULL,
   "thumbnail" varchar(255) DEFAULT NULL,
-  PRIMARY KEY ("userId")
-  /*KEY "userId" ("userId"),
-  KEY "typeId" ("typeId"),
-  CONSTRAINT "useradditionalinfo_ibfk_1" FOREIGN KEY ("userId") REFERENCES "users" ("userId")
+  PRIMARY KEY ("UserId")
+  /*KEY "userId" ("UserId"),
+  KEY "typeId" ("YypeId"),
+  CONSTRAINT "useradditionalinfo_ibfk_1" FOREIGN KEY ("UserId") REFERENCES "users" ("userid")
 */);
 /*Table structure for table "users" */
 
 DROP TABLE IF EXISTS "users";
 
 CREATE TABLE "users" (
-  "userId" SERIAL NOT NULL,
-  "fName" varchar(255) NOT NULL,
-  "lName" varchar(255) NOT NULL,
+  "userid" SERIAL NOT NULL,
+  "fname" varchar(255) NOT NULL,
+  "lname" varchar(255) NOT NULL,
   "username" varchar(255) NOT NULL,
   "password" varchar(255) NOT NULL,
-  "validate" boolean NOT NULL,
-  PRIMARY KEY ("userId")
+  "validate" boolean DEFAULT NULL,
+  PRIMARY KEY ("userid")
 );
 /*Table structure for table "usertype" */
 
 DROP TABLE IF EXISTS "usertype";
 
 CREATE TABLE "usertype" (
-  "typeId" SERIAL NOT NULL,
-  "status" varchar(255) NOT NULL,
-  PRIMARY KEY ("typeId","status")
+  "TypeId" SERIAL NOT NULL,
+  "Status" varchar(255) NOT NULL,
+  PRIMARY KEY ("TypeId","Status")
 );
 
 /*Data for the table "usertype" */
